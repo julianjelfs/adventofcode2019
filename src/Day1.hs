71f7c2575191d19d272711b4c297c82c11df8c56
Julian Jelfs <julian.jelfs@gmail.com>
2019-12-01 11:34:23 +0000

    whatever

---
+++ src/Day1.hs
@@ -0,0 +1,24 @@
+module Day1
+  ( solve
+  )
+where
+
+import           Data.Maybe                     ( mapMaybe )
+import qualified Data.Set                      as Set
+import           Text.Read                      ( readMaybe )
+
+solve :: IO (Maybe Int)
+solve = do
+  inp <- cycle . mapMaybe readNum . lines <$> readFile "data/day1.txt"
+  return $ firstDup 0 Set.empty inp
+ where
+  readNum :: String -> Maybe Int
+  readNum ('+' : xs) = readMaybe xs
+  readNum str        = readMaybe str
+
+  firstDup :: Int -> Set.Set Int -> [Int] -> Maybe Int
+  firstDup _ _ [] = Nothing
+  firstDup freq prev (x : xs)
+    | Set.member freq prev = Just freq
+    | otherwise            = firstDup (freq + x) (Set.insert freq prev) xs
+
---
+++ src/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import           Day19
+
+main :: IO ()
+main = print . show $ Day19.solution
