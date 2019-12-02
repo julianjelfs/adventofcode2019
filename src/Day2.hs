fe03442403332e8dd08219892f6b01afe0a1348e
Julian Jelfs <julian.jelfs@ef.com>
2019-12-02 09:35:01 +0000

    day2 part one

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,18 +1,33 @@
 module Day2
   ( solve
+  , toVector
+  , processInstructions
+  , updateVector
   )
 where
 
-import           Common                         ( mapLines )
-import           Text.Read                      ( readMaybe )
+import           Data.List.Split                ( splitOn )
+import           Data.Vector                   as V
 
-solve :: IO (Maybe Int)
+solve :: IO Int
 solve = do
-  l <- mapLines (fmap fuel . readMaybe) "data/day2.txt"
-  pure $ fmap sum . sequence $ l
+  v <- toVector <$> readFile "data/day2.txt"
+  pure $ processInstructions (v // [(1, 12), (2, 2)])
 
-fuel :: Int -> Int
-fuel n | f > 0     = f + fuel f
-       | otherwise = 0
-  where f = (n `div` 3) - 2
+processInstructions :: V.Vector Int -> Int
+processInstructions v = go v 0
+ where
+  go v' i = case v' ! i of
+    1 -> go (updateVector v' i (+)) (i + 4)
+    2 -> go (updateVector v' i (*)) (i + 4)
+    _ -> v' ! 0
 
+toVector :: String -> V.Vector Int
+toVector = V.fromList . fmap read . splitOn ","
+
+updateVector :: V.Vector Int -> Int -> (Int -> Int -> Int) -> V.Vector Int
+updateVector v i binop = v // [(t, binop l r)]
+ where
+  l = v ! (v ! (i + 1))
+  r = v ! (v ! (i + 2))
+  t = v ! (i + 3)
