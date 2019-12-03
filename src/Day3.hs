a26e00dd54cf8cf5dc7059ec85b4e370f49a53a9
Julian Jelfs <julian.jelfs@ef.com>
2019-12-03 10:42:52 +0000

    day 3 part two

--- src/Day3.hs
+++ src/Day3.hs
@@ -15,7 +15,15 @@ partOne :: IO (Maybe Int)
 partOne = solve <$> input
  where
   solve = S.lookupMin . S.map manhattanDistance . intersection . fmap
-    (path (0, 0) S.empty)
+    (S.fromList . path (0, 0) [])
+
+partTwo :: IO (Maybe Int)
+partTwo = do
+  v <- input
+  let paths@[one, two] = path (0, 0) [] <$> v
+      ints             = intersection $ S.fromList <$> paths
+      c                = combinedSteps one two ints
+  pure $ S.lookupMin c
 
 intersection :: [Set (Int, Int)] -> Set (Int, Int)
 intersection [a, b] = S.intersection a b
@@ -24,21 +32,24 @@ intersection _      = S.empty
 manhattanDistance :: (Int, Int) -> Int
 manhattanDistance (x, y) = abs x + abs y
 
-partTwo :: IO Int
-partTwo = do
-  v <- input
-  pure undefined
+combinedSteps :: [(Int, Int)] -> [(Int, Int)] -> Set (Int, Int) -> Set Int
+combinedSteps one two = S.map (\c -> stepsTo 1 c one + stepsTo 1 c two)
+
+stepsTo :: Int -> (Int, Int) -> [(Int, Int)] -> Int
+stepsTo s _ [] = s
+stepsTo s c (h : t) | h == c    = s
+                    | otherwise = stepsTo (s + 1) c t
 
 toInstructions :: String -> [Instr]
 toInstructions = fmap parse . splitOn ","
   where parse i = Instr (head i) (read (tail i))
 
-path :: (Int, Int) -> Set (Int, Int) -> [Instr] -> Set (Int, Int)
+path :: (Int, Int) -> [(Int, Int)] -> [Instr] -> [(Int, Int)]
 path _      sofar []              = sofar
 path (x, y) sofar (Instr c d : r) = case c of
-  'R' -> path (x + d, y) (merge $ (, y) <$> [(x + 1) .. (x + d)]) r
-  'U' -> path (x, y + d) (merge $ (x, ) <$> [(y + 1) .. (y + d)]) r
-  'D' -> path (x, y - d) (merge $ (x, ) <$> [(y - d) .. (y - 1)]) r
-  'L' -> path (x - d, y) (merge $ (, y) <$> [(x - d) .. (x - 1)]) r
+  'R' -> path (x + d, y) (append $ (, y) <$> [(x + 1) .. (x + d)]) r
+  'U' -> path (x, y + d) (append $ (x, ) <$> [(y + 1) .. (y + d)]) r
+  'D' -> path (x, y - d) (append $ (x, ) <$> reverse [(y - d) .. (y - 1)]) r
+  'L' -> path (x - d, y) (append $ (, y) <$> reverse [(x - d) .. (x - 1)]) r
   _   -> path (x, y) sofar r
-  where merge = S.union sofar . S.fromList
+  where append c = sofar <> c
